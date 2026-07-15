ed1b2d86ceedbf95ac184dc802e3a790a70cf022
Jamie Wilson <howemanning@gmail.com>
2026-07-16 02:26:58 +1200

    Remove laptop neon outline

--- script.js
+++ script.js
@@ -236,15 +236,6 @@ if (heroModelCanvas) {
       let footballIsInView = true;
       let footballFallbackTimer;
       let lastFootballTime = -1;
-      let neonStartTime;
-      let lastScreenFrameTime = 0;
-      const neonOutlineLength = 3406.23;
-      const neonRevealDuration = 2600;
-      const neonHoldDuration = 300;
-      const neonFadeDuration = 850;
-      const neonAnimationDuration = neonRevealDuration + neonHoldDuration + neonFadeDuration;
-      // Traced from the transparent edge of the 365 × 937 wallpaper portrait.
-      const neonOutlinePath = typeof Path2D === "function" ? new Path2D("M189.85,5.87 L202.63,9.03 L214.05,15.62 L223.31,24.99 L229.25,36.74 L232.08,49.63 L234.46,62.62 L237.17,75.49 L235.05,88.52 L231.32,101.19 L226.94,113.65 L226.26,126.56 L236.79,134.14 L248.93,139.39 L261.04,144.67 L273.08,150.14 L285.20,155.41 L296.62,162.00 L305.38,171.82 L311.99,183.26 L317.31,195.36 L321.54,207.88 L325.32,220.54 L328.85,233.28 L332.26,246.06 L335.68,258.82 L338.96,271.63 L340.76,284.66 L339.18,297.71 L342.96,310.36 L346.27,323.15 L349.09,336.06 L351.66,349.02 L353.06,362.17 L353.53,375.38 L353.00,388.58 L352.29,401.79 L352.30,415.00 L353.11,428.19 L352.85,441.40 L353.80,454.58 L355.90,467.62 L356.24,480.80 L353.44,493.70 L346.67,504.90 L336.01,512.64 L323.53,516.14 L322.86,504.96 L323.67,495.37 L313.49,492.25 L312.71,479.08 L313.29,465.88 L315.65,452.90 L320.63,440.65 L322.42,427.64 L320.81,414.53 L317.63,401.70 L313.41,389.17 L309.02,376.71 L305.68,363.92 L303.57,350.88 L301.33,337.85 L298.60,324.91 L295.06,312.19 L289.30,300.31 L283.26,299.66 L282.33,312.85 L282.02,326.07 L282.26,339.28 L283.51,352.43 L285.10,365.56 L287.28,378.59 L290.54,391.39 L292.77,404.42 L293.38,417.62 L293.89,430.82 L294.90,444.01 L296.75,457.10 L297.16,470.28 L295.57,483.41 L294.37,496.56 L292.98,509.71 L291.17,522.81 L288.95,535.83 L285.78,548.67 L282.54,561.48 L280.32,574.50 L279.27,587.68 L279.86,600.87 L281.69,613.96 L283.77,627.02 L285.28,640.15 L285.91,653.35 L285.47,666.55 L283.52,679.62 L280.87,692.57 L278.83,705.64 L277.68,718.80 L276.64,731.98 L275.36,745.13 L271.52,757.71 L264.05,768.57 L264.29,781.50 L266.40,794.50 L264.48,807.52 L258.18,819.08 L252.70,831.05 L248.65,843.62 L240.16,853.38 L227.19,854.45 L214.61,850.56 L206.70,840.62 L207.64,827.50 L208.61,814.34 L207.87,801.14 L208.86,788.01 L212.78,775.39 L213.41,762.30 L211.03,749.31 L213.54,736.45 L215.90,723.56 L214.30,710.44 L212.29,697.41 L200.97,698.13 L189.26,704.22 L185.85,716.36 L187.70,729.45 L188.98,742.61 L189.94,755.79 L190.64,768.99 L189.98,782.19 L190.45,795.36 L192.89,808.36 L191.94,821.46 L189.69,834.45 L190.75,847.61 L192.56,860.70 L195.66,873.55 L196.05,886.73 L195.99,899.94 L195.82,913.13 L188.67,923.85 L176.80,929.48 L163.75,931.50 L150.54,931.86 L137.46,930.31 L129.73,920.58 L131.13,907.57 L138.04,896.35 L141.78,883.83 L145.97,871.47 L151.62,859.80 L148.72,846.94 L143.27,835.03 L136.38,823.98 L135.99,810.79 L132.45,798.15 L127.73,785.82 L125.28,772.84 L123.21,759.79 L120.41,746.86 L117.68,733.93 L115.70,720.87 L114.63,707.69 L114.12,694.49 L113.14,681.31 L111.76,668.17 L110.28,655.04 L108.14,641.99 L105.96,628.96 L102.84,616.12 L100.47,603.12 L98.71,590.02 L97.11,576.89 L95.39,563.78 L93.33,550.73 L91.21,537.68 L89.26,524.61 L86.35,511.72 L83.64,498.80 L83.11,485.60 L84.20,472.44 L86.24,459.38 L86.94,446.19 L87.18,432.97 L88.25,419.80 L90.09,406.71 L90.39,393.51 L90.07,380.29 L90.58,367.09 L91.97,353.94 L92.32,340.74 L91.43,327.55 L90.24,314.38 L87.75,301.44 L80.79,309.80 L75.21,321.78 L71.88,334.56 L69.34,347.52 L67.36,360.60 L65.71,373.71 L62.70,386.57 L58.83,399.21 L54.83,411.81 L51.56,424.62 L49.00,437.58 L49.31,450.73 L54.11,463.01 L57.95,475.62 L59.15,488.78 L59.49,501.99 L55.70,514.48 L52.06,527.08 L40.04,528.14 L28.62,521.53 L19.60,512.00 L14.65,499.79 L12.72,486.73 L13.87,473.59 L16.15,460.57 L16.40,447.37 L16.47,434.16 L16.85,420.95 L16.44,407.74 L15.88,394.52 L15.33,381.31 L15.61,368.11 L17.32,355.00 L20.08,342.08 L23.05,329.20 L27.21,316.66 L28.55,303.72 L26.45,290.82 L29.31,277.91 L32.88,265.19 L36.50,252.47 L40.10,239.75 L43.41,226.96 L46.75,214.17 L50.78,201.59 L56.34,189.60 L63.31,178.39 L72.97,169.43 L84.18,162.45 L96.07,156.67 L107.85,150.68 L119.63,144.68 L131.40,138.66 L142.45,131.50 L144.89,119.13 L141.40,106.40 L136.46,94.15 L133.89,81.19 L134.63,68.13 L139.00,55.68 L141.59,42.72 L146.06,30.31 L153.97,19.79 L164.46,11.82 L176.74,7.02 Z") : null;
       screenCanvas.width = width;
       screenCanvas.height = height;
       staticScreenCanvas.width = width;
@@ -362,44 +353,6 @@ if (heroModelCanvas) {
         context.fill();
       };
 
-      const drawNeonOutline = (time = performance.now()) => {
-        if (reducedMotion || !neonOutlinePath || neonStartTime === undefined || !wallpaperLayout) return;
-
-        const elapsed = time - neonStartTime;
-        if (elapsed < 0 || elapsed >= neonAnimationDuration) return;
-
-        const revealProgress = Math.min(elapsed / neonRevealDuration, 1);
-        const easedReveal = revealProgress < 0.5
-          ? 4 * revealProgress ** 3
-          : 1 - ((-2 * revealProgress + 2) ** 3) / 2;
-        const fadeStart = neonRevealDuration + neonHoldDuration;
-        const opacity = elapsed <= fadeStart
-          ? 1
-          : Math.max(0, 1 - (elapsed - fadeStart) / neonFadeDuration);
-        const { drawX, drawY, drawWidth, drawHeight } = wallpaperLayout;
-
-        context.save();
-        context.translate(drawX, drawY);
-        context.scale(drawWidth / wallpaperImage.naturalWidth, drawHeight / wallpaperImage.naturalHeight);
-        context.lineCap = "round";
-        context.lineJoin = "round";
-        context.setLineDash([neonOutlineLength, neonOutlineLength]);
-        context.lineDashOffset = neonOutlineLength * (1 - easedReveal);
-
-        context.strokeStyle = `rgba(255, 255, 255, ${0.045 * opacity})`;
-        context.lineWidth = 5;
-        context.shadowColor = `rgba(255, 255, 255, ${0.22 * opacity})`;
-        context.shadowBlur = 14;
-        context.stroke(neonOutlinePath);
-
-        context.strokeStyle = `rgba(255, 255, 255, ${0.42 * opacity})`;
-        context.lineWidth = 0.9;
-        context.shadowColor = `rgba(255, 255, 255, ${0.28 * opacity})`;
-        context.shadowBlur = 4;
-        context.stroke(neonOutlinePath);
-        context.restore();
-      };
-
       const drawFallbackIcon = (app, x, y, size, compact = false) => {
         const fontSize = compact ? size * 0.38 : size * (app.short.length > 2 ? 0.28 : 0.43);
         context.font = `700 ${fontSize}px Arial, sans-serif`;
@@ -452,11 +405,10 @@ if (heroModelCanvas) {
         });
       };
 
-      const drawScreenFrame = (time = performance.now()) => {
+      const drawScreenFrame = () => {
         context.clearRect(0, 0, width, height);
         context.drawImage(staticScreenCanvas, 0, 0);
         drawFootball();
-        drawNeonOutline(time);
       };
 
       const cacheStaticScreen = () => {
@@ -466,29 +418,10 @@ if (heroModelCanvas) {
         drawScreenFrame();
       };
 
-      const updateScreenFrame = (time = performance.now()) => {
-        drawScreenFrame(time);
-        lastScreenFrameTime = time;
+      const updateFootballFrame = () => {
+        drawScreenFrame();
         texture.needsUpdate = true;
         requestRender();
-      };
-
-      const startNeonAnimation = () => {
-        if (reducedMotion || !neonOutlinePath || neonStartTime !== undefined) return;
-
-        neonStartTime = performance.now();
-
-        const tick = (time) => {
-          if (time - lastScreenFrameTime > 24) updateScreenFrame(time);
-
-          if (time - neonStartTime < neonAnimationDuration) {
-            requestAnimationFrame(tick);
-          } else {
-            updateScreenFrame(time);
-          }
-        };
-
-        requestAnimationFrame(tick);
       };
 
       const updateFootballVisibility = () => {
@@ -507,7 +440,6 @@ if (heroModelCanvas) {
         cacheStaticScreen();
         texture.needsUpdate = true;
         requestRender();
-        startNeonAnimation();
       };
       wallpaperImage.src = "assets/laptop-wallper.png";
 
@@ -521,13 +453,13 @@ if (heroModelCanvas) {
       });
 
       footballVideo.addEventListener("loadeddata", () => {
-        updateScreenFrame();
+        updateFootballFrame();
 
         if (reducedMotion) return;
 
         if ("requestVideoFrameCallback" in footballVideo) {
-          const handleFootballFrame = (time) => {
-            updateScreenFrame(time);
+          const handleFootballFrame = () => {
+            updateFootballFrame();
             footballVideo.requestVideoFrameCallback(handleFootballFrame);
           };
           footballVideo.requestVideoFrameCallback(handleFootballFrame);
@@ -537,7 +469,7 @@ if (heroModelCanvas) {
             footballFallbackTimer = window.setInterval(() => {
               if (footballVideo.currentTime !== lastFootballTime) {
                 lastFootballTime = footballVideo.currentTime;
-                updateScreenFrame();
+                updateFootballFrame();
               }
             }, 1000 / 60);
           };
